72d24057025b8d7740fbfbc901a53f959c9c4cbc
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-08-31 23:46:45 +0600

    3. pattern j.java

--- Java pattern/3. pattern j.java
+++ Java pattern/3. pattern j.java
@@ -1,6 +1,46 @@
 public class Main {
     public static void main(String[] args) {
+        int n = 5;
 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i >= 1 && i <= 3) {
+                    if (i==1){
+                        if (j >= 2 && j <= 4) {
+                            System.out.print("*  ");
+                        } else {
+                            System.out.print("  ");
+                        }
+                    } else if (j >= 1 && j <= 4) {
+                        System.out.print("  ");
+                    } else {
+                        System.out.print("* ");
+                    }
+                }
+                //another
+
+                else if (i==4) {
+                    if (j == 2 || j == 3 || j==5) {
+                        System.out.print("* ");
+                    } else {
+                        System.out.print("  ");
+                    }
+                }
+                // new
+
+
+                else if (i==5){
+                    if (j==1 || j==5){
+                        System.out.print("  ");
+                    } else {
+                        System.out.print("* ");
+                    }
+                } else{
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
 
 
     }
